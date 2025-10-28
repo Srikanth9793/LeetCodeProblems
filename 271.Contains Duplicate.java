ba50279a686305e16f57006c80a884299ffda0eb
Srikanth9793 <92594118+Srikanth9793@users.noreply.github.com>
2025-10-28 00:02:45 -0700

    Update 271.Contains Duplicate.java
    Added main method. I am using Hashset to find the duplicates

--- 271.Contains Duplicate.java
+++ 271.Contains Duplicate.java
@@ -1,15 +1,28 @@
-class Solution {
+import java.util.HashSet;
+
+public class Solution {
+
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> values = new HashSet<>();
 
-        for(int num : nums) {
+        for (int num : nums) {
             if (values.contains(num)) {
                 return true;
             }
-
             values.add(num);
         }
 
         return false;
+    }
+
+    // Main method to test the function
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {1, 2, 3, 1};
+
+        System.out.println(sol.containsDuplicate(nums1)); // false
+        System.out.println(sol.containsDuplicate(nums2)); // true
     }
 }
